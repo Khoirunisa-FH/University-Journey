02b52ae9c6c643befd76ffc483d1e06f71e63cb4
Khoirunisa-FH <nncaa08@gmail.com>
2024-09-08 18:44:50 +0700

    Update Tugas4 BigData Week 3.py

--- Praktikum Big Data/Tugas BigData Week 3/Tugas4 BigData Week 3.py
+++ Praktikum Big Data/Tugas BigData Week 3/Tugas4 BigData Week 3.py
@@ -1,9 +1,13 @@
 from collections import defaultdict
 
+# Memetakan setiap kata menjadi pasangan (kata, 1)
+
 
 def map_function(text):
     for word in text.split():
         yield (word, 1)
+
+# Menghitung jumlah kemunculan setiap kata
 
 
 def reduce_function(pairs):
@@ -13,11 +17,14 @@ def reduce_function(pairs):
     return result
 
 
-file = open("Dataset Pelatihan.csv", "r")
+# Membaca isi file 'mulai.txt'
+file = open("Dataset pelatihan.csv", "r")
 isi_file = file.read()
 file.close()
 
+# Memproses map dan reduce
 mapped = list(map_function(isi_file))
-
 reduced = reduce_function(mapped)
+
+# Mencetak hasil akhir
 print(dict(reduced))

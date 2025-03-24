c0cb2f875e421d122d47a05d508deecb1c8f217f
Khoirunisa-FH <nncaa08@gmail.com>
2025-03-24 16:10:50 +0700

    Update proses.php

--- Praktikum Web/Mencoba PHP/Codingan/proses.php
+++ Praktikum Web/Mencoba PHP/Codingan/proses.php
@@ -14,3 +14,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
  $conn->close();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SweetAlert2 Berhasil</title>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #b3b3b3;
+        }
+    </style>
+</head>
+<body>
+<script>
+    Swal.fire({
+        icon: "success",
+        text: "Data berhasil disimpan!",
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "OK"
+    });
+</script>
+</body>
+</html>
